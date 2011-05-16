0bae9a2a3aee2b456c0b14f7e79693f1757e1b04
Luca <luca@mac-di-luca.workgroup>
2011-05-16 10:25:54 +0000

    Initial Commit

---
+++ MTG Deck/MTG_DeckAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  MTG_DeckAppDelegate.h
+//  MTG Deck
+//
+//  Created by Luca on 16/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTG_DeckAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
